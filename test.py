3d80bcdb49a6c48cd3c0613c84436bc375a02d5b
mitsu3291 <mitsu3291@users.noreply.github.com>
2024-05-19 11:56:55 +0000

    Apply Code Formatter Change

--- test.py
+++ test.py
@@ -3,16 +3,20 @@ import random
 
 import math
 
+
 def greet(name):
     message = "Hello, " + name
     print(message)
 
+
 name = "Alice"
 greet(name)
 
+
 def add_numbers(a, b):
-  result = a + b
-  return result
+    result = a + b
+    return result
+
 
 num1 = 5
 num2 = 7
